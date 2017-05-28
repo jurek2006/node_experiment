afb95b6197c3d4ab059e4a8d07c0e6bec741f2de
Jurek <jurek2006@gmail.com>
2017-05-28 18:56:52 +0200

    Pobieranie pliku z internetu za pomocą node

--- test.js
+++ test.js
@@ -1,5 +1,17 @@
 // do pracy z systemem plików potrzebny jest modył fs
 var filesYo = require('fs');
+// moduł https (gdyby było trzeba można użyć http)
+var https = require('https');
 
 // utworzenie pliku w bieżącym katalogu
 filesYo.writeFile(__dirname + "/index.html", "<h1>HTML rocks</h1>");
+
+// ścieżka do pliku w internecie
+var dogUrl = "https://images-na.ssl-images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg";
+
+// ścieżka, gdzie plik ma być pobrany
+var dogFile = filesYo.createWriteStream(__dirname + "/node-dog.jpg");
+
+var request = https.get(dogUrl, function (response){
+	response.pipe(dogFile);
+});
