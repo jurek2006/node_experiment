0cd687b3f02a68099e9e2e194d857f33c3f3a897
Jurek <jurek2006@gmail.com>
2017-05-28 18:42:53 +0200

    Tworzenie nowego pliku - index.html za pomocą node

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
+// do pracy z systemem plików potrzebny jest modył fs
+var filesYo = require('fs');
 
+// utworzenie pliku w bieżącym katalogu
+filesYo.writeFile(__dirname + "/index.html", "<h1>HTML rocks</h1>");
